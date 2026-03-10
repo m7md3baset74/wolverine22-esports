eaf1d274cba52d4f95d4ef797db7fda5ad51b47d
m7md3baset74 <mohamedabdelbasset265@gmail.com>
2026-03-10 06:49:40 +0200

    Done

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-extrabold leading-tight bg-transparent text-transparent bg-clip-text bg-gradient-to-r from-blue-800/90 to-green-300 animate-pulse ">H O Store Progress Tracker</h1>
+      <h1 className="px-4 text-center text-3xl md:text-7xl font-extrabold leading-tight bg-transparent text-transparent
+       bg-clip-text bg-gradient-to-r from-orange-800/90 to-yellow-200 animate-pulse">Wolverine22 Esports Progress Tracker</h1>
     </main>
   );
 }
